0494b1927d472f6787c78fc70d0c67f9b4ae9957
Louis Laugier <l.laugier@protonmail.com>
2024-02-25 13:33:32 +0100

    wip

--- api/internal/pkg/validate/validate.go
+++ api/internal/pkg/validate/validate.go
@@ -7,19 +7,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 )
 
 func ValidateEmailAddress(email string) error {
-	r, err := http.Post(fmt.Sprintf(`%s/v0/check_email`, os.Getenv("VALIDATOR_BASE_URL")), "application/json", bytes.NewBuffer([]byte(fmt.Sprintf(`
+	r, err := http.Post("http://localhost:8080/v0/check_email", "application/json", bytes.NewBuffer([]byte(fmt.Sprintf(`
 		{
-			"to_email": "%s",
-    		"from_email": "hello@tweeter-id.com",
-    		"hello_name": "mail.tweeter-id.com"
+			"to_email": "%s"
 		}
 	`, email))))
 	if err != nil {
-		log.Println("Error requesting API:", err)
+		log.Println("Error requesting local reacher:", err)
 		return err
 	}
 	defer r.Body.Close()
@@ -27,23 +24,23 @@ func ValidateEmailAddress(email string) error {
 	responseMap := map[string]interface{}{}
 	err = json.NewDecoder(r.Body).Decode(&responseMap)
 	if err != nil {
-		log.Println("Error decoding API response:", err)
+		log.Println("Error decoding response:", err)
 		return err
 	}
 
 	mx, ok := responseMap["mx"]
 	if !ok {
-		return errors.New("'mx' field not present in API response")
+		return errors.New("mx field not present in response")
 	}
 
 	_, ok = mx.(map[string]interface{})["accepts_mail"]
 	if !ok {
-		return errors.New("mx not accepting emails")
+		return errors.New("domain not accepting emails")
 	}
 
 	isReachable, ok := responseMap["is_reachable"]
 	if !ok {
-		return errors.New("'is_reachable' field not present in API response")
+		return errors.New("is_reachable not present in response")
 	}
 
 	if isReachable == "safe" || isReachable == "risky" {
