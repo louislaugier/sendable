97549c20044fb113eb41b385a469d03c2300f594
Louis Laugier <l.laugier@protonmail.com>
2024-03-20 20:06:56 +0100

    wip

--- frontend/app/services/salesforce/pkce.ts
+++ frontend/app/services/salesforce/pkce.ts
@@ -2,15 +2,14 @@ import salesforceApiClientInstance from ".";
 
 export const fetchSalesforcePKCE = async () => {
     try {
-      // Endpoint provided by Salesforce to generate PKCE parameters
-      const endpoint = '/services/oauth2/pkce/generator';
-      const response = await salesforceApiClientInstance.get(endpoint);
-  
-      // We assume the server returns the structure { code_challenge, code_verifier }
-      return response.data;
+        // Endpoint provided by Salesforce to generate PKCE parameters
+        const endpoint = '/services/oauth2/pkce/generator';
+        const response = await salesforceApiClientInstance.get(endpoint);
+
+        // We assume the server returns the structure { code_challenge, code_verifier }
+        return response.data;
     } catch (error) {
-      console.error('Error fetching Salesforce PKCE parameters:', error);
-      throw error; // Re-throw the error to handle it in the calling function
+        console.error('Error fetching Salesforce PKCE parameters:', error);
+        throw error; // Re-throw the error to handle it in the calling function
     }
-  };
-  
+};
