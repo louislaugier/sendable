33975d52e8098e137dafc603556a69eaf3aea25c
Louis Laugier <l.laugier@protonmail.com>
2024-04-09 01:41:56 +0200

    wip

--- frontend/app/routes/api.tsx
+++ frontend/app/routes/api.tsx
@@ -1,3 +1,4 @@
+import { Button, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow } from "@nextui-org/react";
 import type { MetaFunction } from "@remix-run/node";
 import { siteName } from "~/constants/app";
 
@@ -12,6 +13,54 @@ export default function Api() {
 
   return (
     <>
+      <div className="py-8 px-6">
+
+        <div className="flex flex-col items-center mb-8">
+          <p className="text-blue600">API</p>
+          <h2 className="text-2xl">Validate addresses programmatically</h2>
+        </div>
+
+        <Table removeWrapper aria-label="Example static collection table" className="my-16">
+          <TableHeader>
+            <TableColumn>NAME</TableColumn>
+            <TableColumn>ROLE</TableColumn>
+            <TableColumn>STATUS</TableColumn>
+          </TableHeader>
+          <TableBody>
+            <TableRow key="1">
+              <TableCell>Tony Reichert</TableCell>
+              <TableCell>CEO</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="2">
+              <TableCell>Zoey Lang</TableCell>
+              <TableCell>Technical Lead</TableCell>
+              <TableCell>Paused</TableCell>
+            </TableRow>
+            <TableRow key="3">
+              <TableCell>Jane Fisher</TableCell>
+              <TableCell>Senior Developer</TableCell>
+              <TableCell>Active</TableCell>
+            </TableRow>
+            <TableRow key="4">
+              <TableCell>William Howard</TableCell>
+              <TableCell>Community Manager</TableCell>
+              <TableCell>Vacation</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <p className="mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+          nihil, eveniet aliquid culpa officia aut!</p>
+        <Button onClick={() => {
+        }} color="primary" variant="shadow">
+          Open API docs
+        </Button>
+      </div>
     </>
   );
 }
--- frontend/app/routes/resources.tsx
+++ frontend/app/routes/resources.tsx
@@ -1,3 +1,4 @@
+import { Card, CardHeader, Divider, CardBody, CardFooter, Link, Image } from "@nextui-org/react";
 import type { MetaFunction } from "@remix-run/node";
 import { siteName } from "~/constants/app";
 
@@ -10,8 +11,50 @@ export const meta: MetaFunction = () => {
 
 export default function Resources() {
 
+  const test = <Card className="max-w-[400px] mb-8">
+    <CardHeader className="flex gap-3">
+      <div className="flex flex-col">
+        <p className="text-md">NextUI</p>
+        <p className="text-small text-default-500">nextui.org</p>
+      </div>
+    </CardHeader>
+    <Divider />
+    <CardBody>
+      <p>Make beautiful websites regardless of your design experience.</p>
+    </CardBody>
+    <Divider />
+    <CardFooter>
+      <Link
+        isExternal
+        showAnchorIcon
+        href="https://github.com/nextui-org/nextui"
+      >
+        Visit source code on GitHub.
+      </Link>
+    </CardFooter>
+  </Card>
+
   return (
-    <>
-    </>
+    <div className="py-8 px-6">
+
+      <div className="flex flex-col items-center mb-16">
+        <p className="text-blue600">Resources</p>
+        <h2 className="text-2xl">Articles and tutorials</h2>
+      </div>
+
+      <div className="flex flex-wrap justify-between">
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+        {test}
+      </div>
+    </div>
   );
 }
