b61ded296353445909cb3dc098828f0d929c3b07
Louis Laugier <l.laugier@protonmail.com>
2024-04-17 01:57:38 +0200

    wip

--- api/handlers/auth_zoho_confirm_email.go
+++ api/handlers/auth_zoho_confirm_email.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 )
 
+// We can get organization info via oauth but no info on the user logging in, therefore we need to ask the user who he is in a given organization after the oauth flow and then send him a confirmation email
 func zohoAuthConfirmEmailHandler(w http.ResponseWriter, r *http.Request) {
 	// body := models.ZohoAuthRequest{}
 
