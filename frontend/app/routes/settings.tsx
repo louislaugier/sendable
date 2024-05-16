db98ed8e1cc82f293673194038fba6102d7e0db3
Louis Laugier <l.laugier@protonmail.com>
2024-05-17 01:02:25 +0200

    wip

--- frontend/app/routes/settings.tsx
+++ frontend/app/routes/settings.tsx
@@ -1,4 +1,8 @@
+import { Tabs, Tab } from "@nextui-org/react";
 import type { MetaFunction } from "@remix-run/node";
+import { useState } from "react";
+import EmailValidatorTab from "~/components/EmailValidatorTab";
+import ValidationHistoryTable from "~/components/Tables/ValidationHistoryTable";
 import { siteName } from "~/constants/app";
 
 export const meta: MetaFunction = () => {
@@ -9,6 +13,7 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Settings() {
+  const [selectedTab, setSelectedTab] = useState<any>("user");
 
   return (
     <>
@@ -17,6 +22,55 @@ export default function Settings() {
         <div className="flex flex-col items-center mb-16">
           <h2 className="text-2xl">Settings</h2>
         </div>
+
+        <Tabs
+          aria-label="Options"
+          color="primary"
+          variant="bordered"
+          selectedKey={selectedTab}
+          onSelectionChange={setSelectedTab}
+        >
+          <Tab
+            key="user"
+            title={
+              <div className="flex items-center space-x-2">
+                <span>User</span>
+              </div>
+            }
+          >
+
+          </Tab>
+          <Tab
+            key="plan"
+            title={
+              <div className="flex items-center space-x-2">
+                <span>Plan</span>
+              </div>
+            }
+          >
+
+          </Tab>
+          <Tab
+            key="integrations"
+            title={
+              <div className="flex items-center space-x-2">
+                <span>Integrations</span>
+              </div>
+            }
+          >
+
+          </Tab>
+          <Tab
+            key="api"
+            title={
+              <div className="flex items-center space-x-2">
+                <span>API</span>
+              </div>
+            }
+          >
+
+          </Tab>
+        </Tabs>
 
         <div className="flex flex-wrap justify-between">
 
