cbb8ca6b6ad39d5c614ff9094e601ac95172c472
Louis Laugier <l.laugier@protonmail.com>
2024-05-29 01:51:11 +0200

    wip

--- api/internal/models/template.go
+++ api/internal/models/template.go
@@ -3,7 +3,7 @@ package models
 type Template string
 
 const (
-	ConfirmEmailTemplate Template = "confirm"
-	ReportTemplate       Template = "report"
+	ConfirmEmailTemplate Template = "confirm_email_address"
+	ReportTemplate       Template = "email_validation_report"
 	ErrorTemplate        Template = "error"
 )
