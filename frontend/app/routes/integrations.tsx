3773a8f382ba2536b2c74b0f4e642608901f15b8
Louis Laugier <l.laugier@protonmail.com>
2024-06-01 13:45:29 +0200

    --wip-- [skip ci]

--- frontend/app/routes/integrations.tsx
+++ frontend/app/routes/integrations.tsx
@@ -5,10 +5,10 @@ import HubspotFullLogo from "~/components/icons/logos/HubspotFullLogo";
 import SalesforceFullLogo from "~/components/icons/logos/SalesforceFullLogo";
 import SendgridFullLogo from "~/components/icons/logos/SendgridFullLogo";
 import ZohoFullLogo from "~/components/icons/logos/ZohoFullLogo";
+import IntegrationCard from "~/components/cards/IntegrationCard";
 import HubspotAuthButton from "~/components/single_components/AuthButtons/HubspotAuthButton";
 import SalesforceAuthButton from "~/components/single_components/AuthButtons/SalesforceAuthButton";
 import ZohoAuthButton from "~/components/single_components/AuthButtons/ZohoAuthButton";
-import IntegrationCard from "~/components/cards/IntegrationCard";
 
 export const meta: MetaFunction = () => {
   return [
