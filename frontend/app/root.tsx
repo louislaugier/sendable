f5770dc133cffe5de1d160afe4b5e116b88e86c2
louislaugier <l.laugier@protonmail.com>
2025-05-24 22:51:23 +0200

    --wip-- [skip ci]

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -50,6 +50,21 @@ export function Layout({ children }: { children: React.ReactNode }) {
             }
           })();
         `}} />
+
+        {/* Brevo Conversations {literal} */}
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(d, w, c) {
+            w.BrevoConversationsID = '67475253fa2d4fbaf909695f';
+            w[c] = w[c] || function() {
+                (w[c].q = w[c].q || []).push(arguments);
+            };
+            var s = d.createElement('script');
+            s.async = true;
+            s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+            if (d.head) d.head.appendChild(s);
+          })(document, window, 'BrevoConversations');`
+        }} />
+
       </head>
       <body>
         <ErrorOccurredModalProvider>
